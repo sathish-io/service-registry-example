79a761f186cada090b9a03a32eae1cb69c427919
Sathishkumar Kandasamy <sathishmail@gmail.com>
2015-09-06 13:21:21 -0700

    Initial chages

--- src/main/java/com/sathish/serviceregistry/ServiceRegistryExampleApplication.java
+++ src/main/java/com/sathish/serviceregistry/ServiceRegistryExampleApplication.java
@@ -1,4 +1,4 @@
-package com.yodlee.serviceregistry;
+package com.sathish.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/sathish/serviceregistry/ServiceRegistryExampleApplicationTests.java
+++ src/test/java/com/sathish/serviceregistry/ServiceRegistryExampleApplicationTests.java
@@ -1,9 +1,11 @@
-package com.yodlee.serviceregistry;
+package com.sathish.serviceregistry;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.sathish.serviceregistry.ServiceRegistryExampleApplication;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ServiceRegistryExampleApplication.class)
